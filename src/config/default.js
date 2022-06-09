38c8753928456c0b4da214b760f8df22752851c7
Jordan Pedersen <jordnpedersen@gmail.com>
2022-06-08 20:30:43 -0400

    style: made code abide to style guide

--- src/config/default.js
+++ src/config/default.js
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 
-const dimensions = {x: 1, y: 1, z:1};
+const scale = {x: 1, y: 1, z: 1};
 
 const material = new THREE.MeshStandardMaterial({
-    color: 0x111111,
-    dithering: true,
-    metalness: 1,
-    opacity: 1,
-    roughtness: 0.5,
-    transparent: true
+  color: 0x111111,
+  dithering: true,
+  metalness: 1,
+  opacity: 1,
+  roughness: 0.5,
+  transparent: true
 });
 
-export { dimensions, material }
+export {scale, material};
