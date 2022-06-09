c5bf3012656c3929c25405167c022f474bdf6787
Jordan Pedersen <jordnpedersen@gmail.com>
2022-06-08 20:35:31 -0400

    fix: change 'dimensions' to 'scale'

--- src/objects/cube.js
+++ src/objects/cube.js
@@ -1,17 +1,17 @@
 import * as THREE from 'three';
-import * as DEFAULT from '/src/config/default.js'
-import {scene} from '/src/main.js'
+import * as DEFAULT from '/src/config/default.js';
+import {scene} from '/src/main.js';
 
 // TODO: Make an object constructor for the object properties
 // TODO: Have parameters for custom properties given by user
 
 /**
- * Adds a cube to the scene.
+ * Adds a cube to the scene
  */
 function addCube() {
-    const geometry = new THREE.BoxGeometry( DEFAULT.dimensions.x, DEFAULT.dimensions.y, DEFAULT.dimensions.z );
-    const cube = new THREE.Mesh( geometry, DEFAULT.material );
-    scene.add( cube );
+  const geometry = new THREE.BoxGeometry(DEFAULT.scale.x, DEFAULT.scale.y, DEFAULT.scale.z);
+  const cube = new THREE.Mesh(geometry, DEFAULT.material);
+  scene.add(cube);
 }
 
-export { addCube }
+export {addCube};
