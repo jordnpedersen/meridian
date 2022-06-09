65ed375f4b697f088f1f8bc20501c89c6a242d44
dlnet <ben.ray.com@gmail.com>
2022-06-08 23:06:20 -0600

    feat: implement knot object

---
+++ src/object/knot.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import * as THREE from 'three';
+import * as STANDARD from '/src/material/standard.js';
+import {scene} from '/src/main.js';
+
+const geometry = new THREE.TorusKnotGeometry(.35, .1, 64, 16);
+
+/**
+ * Adds a knot object to the scene
+ */
+function addKnot() {
+  const knot = new THREE.Mesh(geometry, STANDARD.material);
+  scene.add(knot);
+}
+
+export {addKnot};
