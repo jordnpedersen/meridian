eea4fffac8cb421b1e4ab2cbdc0592faea704dc6
dlnet <ben.ray.com@gmail.com>
2022-06-15 12:44:14 -0600

    docs: add ui documentation

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -4,29 +4,54 @@ import * as TRANSFORM from '/src/controls/transform.js';
 import {scene} from '/src/main.js';
 import {ui} from '/src/configs/ui.js';
 
+/**
+ * Updates UI position
+ * @param {float} x x position
+ * @param {float} y y position
+ * @param {z} z z position
+ */
 function updateUIposition(x, y, z) {
   ui.position.x.value = x;
   ui.position.y.value = y;
   ui.position.z.value = z;
 }
 
+/**
+ * Updates UI rotation
+ * @param {float} x x rotation
+ * @param {float} y y rotation
+ * @param {float} z z rotation
+ */
 function updateUIrotation(x, y, z) {
   ui.rotation.x.value = x;
   ui.rotation.y.value = y;
   ui.rotation.z.value = z;
 }
 
+/**
+ * Updates UI scale
+ * @param {float} x x scale
+ * @param {float} y y scale
+ * @param {float} z z scale
+ */
 function updateUIscale(x, y, z) {
   ui.scale.x.value = x;
   ui.scale.y.value = y;
   ui.scale.z.value = z;
 }
 
+/**
+ * Updates material color of 'object'
+ * @param {THREE.Object3D} object
+ */
 function updateMaterialColor(object) {
   const color = ui.color.value.replace('#', '0x');
   object.material.color.setHex(color);
 }
 
+/**
+ * Resets UI to default parameters
+ */
 function resetUI() {
   updateUIposition(0, 0, 0);
   updateUIrotation(0, 0, 0);
@@ -34,6 +59,9 @@ function resetUI() {
 
 }
 
+/**
+ * Updates UI. Should probably seperate functionality with updating material color.
+ */
 function updateUI() {
   if (TRANSFORM.controls.object === undefined) {
     resetUI();
@@ -45,6 +73,9 @@ function updateUI() {
   }
 }
 
+/**
+ * Attaches transform controls to selected UI object from scene
+ */
 document.getElementById('sceneObjects').addEventListener('click', event => {
   const object = scene.getObjectById(parseInt(event.target.id));
   if (object !== undefined) {
