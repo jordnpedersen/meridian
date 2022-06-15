a44c86c168dcc80f3ef55bbb27f9370210ddcb08
dlnet <ben.ray.com@gmail.com>
2022-06-15 12:40:02 -0600

    feat: use ui configs and add material update

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -2,52 +2,51 @@
 
 import * as TRANSFORM from '/src/controls/transform.js';
 import {scene} from '/src/main.js';
-
-const px = document.getElementById('position-x');
-const py = document.getElementById('position-y');
-const pz = document.getElementById('position-z');
-
-const rx = document.getElementById('rotation-x');
-const ry = document.getElementById('rotation-y');
-const rz = document.getElementById('rotation-z');
-
-const sx = document.getElementById('scale-x');
-const sy = document.getElementById('scale-y');
-const sz = document.getElementById('scale-z');
+import {ui} from '/src/configs/ui.js';
 
 function updateUIposition(x, y, z) {
-  px.value = x;
-  py.value = y;
-  pz.value = z;
+  ui.position.x.value = x;
+  ui.position.y.value = y;
+  ui.position.z.value = z;
 }
 
 function updateUIrotation(x, y, z) {
-  rx.value = x;
-  ry.value = y;
-  rz.value = z;
+  ui.rotation.x.value = x;
+  ui.rotation.y.value = y;
+  ui.rotation.z.value = z;
 }
 
 function updateUIscale(x, y, z) {
-  sx.value = x;
-  sy.value = y;
-  sz.value = z;
+  ui.scale.x.value = x;
+  ui.scale.y.value = y;
+  ui.scale.z.value = z;
+}
+
+function updateMaterialColor(object) {
+  const color = ui.color.value.replace('#', '0x');
+  object.material.color.setHex(color);
+}
+
+function resetUI() {
+  updateUIposition(0, 0, 0);
+  updateUIrotation(0, 0, 0);
+  updateUIscale(0, 0, 0);
+
 }
 
 function updateUI() {
   if (TRANSFORM.controls.object === undefined) {
-    updateUIposition(0, 0, 0);
-    updateUIrotation(0, 0, 0);
-    updateUIscale(0, 0, 0);
+    resetUI();
   } else {
     updateUIposition(TRANSFORM.controls.object.position.x, TRANSFORM.controls.object.position.y, TRANSFORM.controls.object.position.z);
     updateUIrotation(TRANSFORM.controls.object.rotation.x, TRANSFORM.controls.object.rotation.y, TRANSFORM.controls.object.rotation.z);
     updateUIscale(TRANSFORM.controls.object.scale.x, TRANSFORM.controls.object.scale.y, TRANSFORM.controls.object.scale.z);
+    updateMaterialColor(TRANSFORM.controls.object);
   }
 }
 
 document.getElementById('sceneObjects').addEventListener('click', event => {
   const object = scene.getObjectById(parseInt(event.target.id));
-
   if (object !== undefined) {
     TRANSFORM.controls.attach(object);
   }
