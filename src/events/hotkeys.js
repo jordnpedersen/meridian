bdb28ee630a174cd40ba6f878ea838361f3b25a5
dlnet <ben.ray.com@gmail.com>
2022-06-23 12:48:18 -0600

    fix: use proper naming conventions

--- src/events/hotkeys.js
+++ src/events/hotkeys.js
@@ -2,57 +2,56 @@
 
 import * as THREE from 'three';
 import * as ORBIT from '/src/controls/orbit.js';
-import {scene, camera, perspective, updateCamera} from '/src/main.js';
-import {raycasterObjects} from '/src/utils/object.js';
+import * as TRANSFORM from '/src/controls/transform.js';
+import {camera, perspective, updateCamera} from '/src/main.js';
 import {deleteOutline} from '/src/utils/outline.js';
-import {deleteObject} from '/src/utils/object.js';
 
 /**
  * Event listener for transform shortcuts
  */
 document.addEventListener('keydown', event => {
-  if (controls.object !== undefined) {
+  if (TRANSFORM.controls.object !== undefined) {
     switch (event.key) {
       case 'q':
-        controls.setSpace(controls.space === 'local' ? 'world' : 'local');
+        TRANSFORM.controls.setSpace(TRANSFORM.controls.space === 'local' ? 'world' : 'local');
         break;
       case 'w':
-        controls.setMode('translate');
+        TRANSFORM.controls.setMode('translate');
         break;
       case 'e':
-        controls.setMode('rotate');
+        TRANSFORM.controls.setMode('rotate');
         break;
       case 'r':
-        controls.setMode('scale');
+        TRANSFORM.controls.setMode('scale');
         break;
       case 'a':
-        controls.setSize(controls.size + 0.1);
+        TRANSFORM.controls.setSize(TRANSFORM.controls.size + 0.1);
         break;
       case 's':
-        controls.setSize(Math.max(controls.size - 0.1, 0.1));
+        TRANSFORM.controls.setSize(Math.max(TRANSFORM.controls.size - 0.1, 0.1));
         break;
       case 'd':
         const position = camera.position.clone();
         updateCamera();
         camera.position.copy(position);
         camera.lookAt(0, 0, 0);
-        controls.camera = camera;
+        TRANSFORM.controls.camera = camera;
         break;
       case 'f':
-        camera.lookAt(controls.object.position);
-        ORBIT.controls.target.set(controls.object.position.x, controls.object.position.y, controls.object.position.z);
+        camera.lookAt(TRANSFORM.controls.object.position);
+        ORBIT.controls.target.set(TRANSFORM.controls.object.position.x, TRANSFORM.controls.object.position.y, TRANSFORM.controls.object.position.z);
         break;
       case 'z':
-        controls.showZ = !controls.showZ;
+        TRANSFORM.controls.showZ = !TRANSFORM.controls.showZ;
         break;
       case 'x':
-        controls.showX = !controls.showX;
+        TRANSFORM.controls.showX = !TRANSFORM.controls.showX;
         break;
       case 'c':
-        controls.showY = !controls.showY;
+        TRANSFORM.controls.showY = !TRANSFORM.controls.showY;
         break;
       case 'v':
-        controls.enabled = !controls.enabled;
+        TRANSFORM.controls.enabled = !TRANSFORM.controls.enabled;
         break;
       case '-':
       case '_':
@@ -81,28 +80,19 @@ document.addEventListener('keydown', event => {
         camera.updateProjectionMatrix();
         break;
       case 'Shift':
-        controls.setTranslationSnap(0.5);
-        controls.setRotationSnap(THREE.MathUtils.degToRad(15));
-        controls.setScaleSnap(0.5);
+        TRANSFORM.controls.setTranslationSnap(0.5);
+        TRANSFORM.controls.setRotationSnap(THREE.MathUtils.degToRad(15));
+        TRANSFORM.controls.setScaleSnap(0.5);
         break;
       case 'Escape':
-        if (controls.object.isMesh) {
+        if (TRANSFORM.controls.object.isMesh) {
           deleteOutline();
         }
-        controls.detach();
+        TRANSFORM.controls.detach();
         break;
       case 'Delete':
-        if (controls.object.isMesh) {
-          deleteOutline();
-        }
-        if (controls.object.isLight) {
-          const lightHelper = scene.getObjectByName(controls.object.uuid);
-          raycasterObjects.splice(raycasterObjects.findIndex(obj => obj.uuid === lightHelper.uuid), 1);
-        } else {
-          raycasterObjects.splice(raycasterObjects.findIndex(obj => obj.uuid === controls.object.uuid), 1);
-        }
-        deleteObject(controls.object);
-        controls.detach();
+        TRANSFORM.deleteAttachedObject();
+        TRANSFORM.controls.detach();
         break;
     }
   }
@@ -111,9 +101,9 @@ document.addEventListener('keydown', event => {
 document.addEventListener('keyup', event => {
   switch (event.key) {
     case 'Shift':
-      controls.setTranslationSnap(null);
-      controls.setRotationSnap(null);
-      controls.setScaleSnap(null);
+      TRANSFORM.controls.setTranslationSnap(null);
+      TRANSFORM.controls.setRotationSnap(null);
+      TRANSFORM.controls.setScaleSnap(null);
       break;
   }
 });
